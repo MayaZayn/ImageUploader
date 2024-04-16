6141d92e06373fe8fd732418fd87d252d1aff7d6
Maya Zain El-Din <11410120210508@stud.cu.edu.eg>
2024-04-16 20:50:04 +0200

    Enhance Source Code

--- Program.cs
+++ Program.cs
@@ -12,34 +12,35 @@ app.UseStaticFiles();
 app.MapGet("/", (HttpContext context, IAntiforgery antiforgery) =>
 {
     var token = antiforgery.GetAndStoreTokens(context);
-    var html = $@"
+    var html = $"""
         <!DOCTYPE html>
-        <html lang=""en"">
+        <html lang="en">
         <head>
-            <meta charset=""UTF-8"">
-            <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Image Uploader</title>
-            <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"" crossorigin=""anonymous"">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         </head>
         <body>
-            <main class=""container"">
-            <h1 class=""my-3"">Image Uploader</h1>
-            <form action=""upload"" method=""POST"" enctype=""multipart/form-data"">
-                <div class=""mb-3"">
-                <input name=""{token.FormFieldName}"" type=""hidden"" value=""{token.RequestToken}"" />
-                <input name=""title"" type=""text"" class=""form-control"" id=""imageTitle"" placeholder=""Enter the image's title"" required>
+            <main class="container">
+            <h1 class="my-3">Image Uploader</h1>
+            <form action="upload" method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                <input name="{token.FormFieldName}" type="hidden" value="{token.RequestToken}" />
+                <input name="title" type="text" class="form-control" id="imageTitle" placeholder="Enter the image's title" required>
                 </div>
-                <div class=""mb-3"">
-                <label for=""file"" class=""form-label"">Pick an Image</label>
-                <input name=""file"" class=""form-control"" type=""file"" id=""file"" required>
+                <div class="mb-3">
+                <label for="file" class="form-label">Pick an Image</label>
+                <input name="file"" class="form-control" type="file" id="file" required>
                 </div>
-                <button type=""submit"" class=""btn btn-primary"">Upload Image</button>
+                <button type="submit" class="btn btn-primary">Upload Image</button>
             </form>
             </main>
 
-            <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"" integrity=""sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"" crossorigin=""anonymous""></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         </body>
-        </html>";
+        </html>
+        """;
 
     return Results.Content(html, "text/html");
 });
@@ -103,7 +104,7 @@ app.MapPost("/upload", async (
         }
     });
 
-app.MapGet("/picture/{uniqueId}", async (HttpContext context, [FromRoute] string uniqueId) => {
+app.MapGet("/picture/{uniqueId}", async ([FromRoute] string uniqueId) => {
     var jsonPath = Path.Combine(builder.Environment.ContentRootPath, "images.json");
     string json = await File.ReadAllTextAsync(jsonPath);
     var imagesInfo = JsonSerializer.Deserialize<List<ImageInfo>>(json);
@@ -111,19 +112,19 @@ app.MapGet("/picture/{uniqueId}", async (HttpContext context, [FromRoute] string
     string imagePath = null, imageTitle = null;
     foreach (var image in imagesInfo)
     {
-        if (image.Id == uniqueId) {
+        if (image.Id == uniqueId) 
+        {
             imagePath = image.Path;
             imageTitle = image.Title;
             break;
         }
     }
 
-    var html = $@"
-        <h1 style=""text-align:center"">{imageTitle}</h1>
-        <img style=""display:block; margin:auto; width:800px; height:600px"" src=""/{imagePath}"" alt=""{imageTitle}""/>
-    ";
-    // var html = $"<h1 style=\"text-align:center\">{imageTitle}</h1> <img style=\"display:block; margin:auto; width:800px; height:600px\" src=\"/{imagePath}\" alt=\"{imageTitle}\"/>";
-    
+    var html = $"""
+        <h1 style="text-align:center">{imageTitle}</h1>
+        <img style="display:block; margin:auto; width:800px; height:600px" src="/{imagePath}" alt="{imageTitle}"/>
+    """;
+
     return Results.Content(html, "text/html");
 });
 
@@ -133,14 +134,16 @@ app.MapGet("/uploaded/{uniqueId}", async ([FromRoute] string uniqueId) =>
     string json = await File.ReadAllTextAsync(jsonPath);
     var imagesInfo = JsonSerializer.Deserialize<List<ImageInfo>>(json);
 
+    var imgQuery =
+        from img in imagesInfo
+        where img.Id == uniqueId
+        select img;
+
     string imagePath = null, imageExtension = null;
-    foreach (var image in imagesInfo)
+    foreach (var image in imgQuery)
     {
-        if (image.Id == uniqueId) {
-            imagePath = Path.Combine(builder.Environment.ContentRootPath, image.Path + image.Extension);
-            imageExtension = image.Extension;
-            break;
-        }
+        imagePath = Path.Combine(builder.Environment.ContentRootPath, image.Path + image.Extension);
+        imageExtension = image.Extension;
     }
 
     if (File.Exists(imagePath))
